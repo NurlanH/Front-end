9bfcf38c47996402f3015f55906151cbdf5ff9c7
Nurlan Huseynov <nurlanah@code.edu.az>
2019-12-17 21:10:20 +0400

    working on formpage

--- Lahiyye Front_end/Script/MainScript.js
+++ Lahiyye Front_end/Script/MainScript.js
@@ -1,3 +1,4 @@
+
 // When scroll header is fixed
 $(document).scroll(function(){
     $("header").css({"opacity":0});
@@ -37,23 +38,6 @@ $(document).ready(function() {
 });
 
 
-//For main slider
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
-});
-
-
-$('#particle').particleground({
-    dotColor: '#000000',
-    lineColor: '#101820ff'
-});
-
 
 //For navbar
 
@@ -73,3 +57,83 @@ $(".closeNav").click(function(){
 
 
   
+
+// for share post tags input
+$.each(document.getElementsByClassName("tags-input"),function(index,el){
+    let hiddenInput = document.createElement('input'),
+        mainInput = document.createElement('input'),
+        tags = [];
+    
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', el.getAttribute('data-name'));
+
+    mainInput.setAttribute('type', 'text');
+    mainInput.classList.add('main-input');
+    mainInput.addEventListener('keyup', function (e) {
+        if(e.keyCode === 32){
+            let enteredTags = mainInput.value.split(' ');
+            if (enteredTags.length > 1) {
+                enteredTags.forEach(function (t) {
+                    let filteredTag = filterTag(t);
+                    if (filteredTag.length > 0)
+                        addTag(filteredTag);
+                });
+                mainInput.value = '';
+            }
+        }
+        
+    });
+
+    mainInput.addEventListener('keydown', function (e) {
+        let keyCode = e.which || e.keyCode;
+        if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
+            removeTag(tags.length - 1);
+        }
+    });
+
+    el.appendChild(mainInput);
+    el.appendChild(hiddenInput);
+
+
+    function addTag (text) {
+        let tag = {
+            text: text,
+            element: document.createElement('span'),
+        };
+
+        tag.element.classList.add('tag');
+        tag.element.textContent = tag.text;
+
+        let closeBtn = document.createElement('span');
+        closeBtn.classList.add('close');
+        closeBtn.addEventListener('click', function () {
+            removeTag(tags.indexOf(tag));
+        });
+        tag.element.appendChild(closeBtn);
+
+        tags.push(tag);
+
+        el.insertBefore(tag.element, mainInput);
+
+        refreshTags();
+    }
+
+    function removeTag (index) {
+        let tag = tags[index];
+        tags.splice(index, 1);
+        el.removeChild(tag.element);
+        refreshTags();
+    }
+
+    function refreshTags () {
+        let tagsList = [];
+        tags.forEach(function (t) {
+            tagsList.push(t.text);
+        });
+        hiddenInput.value = tagsList.join(',');
+    }
+
+    function filterTag (tag) {
+        return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+    }
+});
